bc61f7956cab46f4b531914df2505d6b1ea37a81
Ovsjah <ovsjah@gmail.com>
2020-09-18 22:19:36 +0300

    Change migration phone type

--- db/migrate/20200917161211_create_users.rb
+++ db/migrate/20200917161211_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :email
-      t.integer :phone
+      t.string :phone
       t.string :password_digest
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_161211) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.integer "phone"
+    t.string "phone"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
